ffb975fc9a877a9c891f5896bfaf1eed643810f0
rikithamanjunath <rikitha.manjunath@sjsu.edu>
2019-11-17 21:04:41 -0800

    fixing folder name

--- data/react-setup.sh
+++ data/react-setup.sh
@@ -1,6 +1,7 @@
 cd /vagrant_data/
 curl -sL https://rpm.nodesource.com/setup_13.x | sudo -E bash -
 yum install -y nodejs
+cd react-http
 sudo npm install -g --force npm@latest yarn@latest react@latest typescript@latest npx@latest axios@latest fsevents@latest core-js@latest left-pad@latest webpack@latest
 npm cache clean --force
 yarn add axios  
