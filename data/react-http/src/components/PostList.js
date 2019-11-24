2e22eb519e00d7b36ed5fb43e76f9bccb3417bbb
rikithamanjunath <rikitha.manjunath@sjsu.edu>
2019-11-23 16:15:20 -0800

    Get request

--- data/react-http/src/components/PostList.js
+++ data/react-http/src/components/PostList.js
@@ -12,16 +12,29 @@ class PostList extends Component {
     }
     // only once when loaded
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('https://jsonplaceholder.typicode.com/posts1')
             .then(response => {
                 console.log(response)
-            })
+                this.setState({posts: response.data})
+            }).catch( error =>{
+                console.log(error);
+                this.setState({errorMessage:"Error fetching data"})
+            }
+
+            )
     }
 
     render() {
+        const {posts,errorMessage} = this.state;
         return (
             <div>
                 List of Posts
+                {
+    posts.length ? posts.map(post => <div key = {post.id}>{post.title}</div>):null 
+                }
+                {
+    errorMessage? <div>{errorMessage}</div>: null
+                }
             </div>
         )
     }
